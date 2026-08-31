dc8f548a5fe1b900879a2087758c410a302ddde5
TheWarrior-tech <220263824+TheWarrior-tech@users.noreply.github.com>
2026-08-31 19:38:30 +0100

    Add files via upload

---
+++ machub/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.atugil.machub',
+  appName: 'Mac Hub',
+  webDir: 'www'
+};
+
+export default config;
