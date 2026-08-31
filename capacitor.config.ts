be184bbae3f7b163274b323f043c930f8d7fbe3f
TheWarrior-tech <220263824+TheWarrior-tech@users.noreply.github.com>
2026-08-31 19:59:13 +0100

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.atugil.machub',
-  appName: 'Mac Hub',
-  webDir: 'www'
-};
-
-export default config;
